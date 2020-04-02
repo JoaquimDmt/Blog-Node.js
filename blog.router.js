8345a2bc0446de73c69d4e39fb1db3abdc1d2638
Joaquim Dmt <joaquim.dimicode@users.noreply.github.com>
2020-04-02 08:42:52 +0200

    redirection vers chaque article + toLocaleDateString() + tri des articles par date de mise en ligne

--- blog.router.js
+++ blog.router.js
@@ -5,12 +5,20 @@ const Article = require('./models/Article.model')
 const Authors = require('./models/Author.model')
 const Category = require('./models/Category.model')
 
-
 blogRouter.get('/', (req, res) =>{
-    Article.find().populate('author category').exec().then(articles => {
+    Article.find().populate('author category').sort({dateCreated: 'desc'}).exec().then(articles => {
         // console.log({articles})
         res.render('index.pug', { articles })
     }).catch(error => res.send(error)) 
+});
+blogRouter.get('/article/:id', async (req, res) =>{
+    await Article.findById(req.params.id).populate('author category').exec().then(article => {
+        if (article == null) {
+            res.redirect('/')
+            // throw "L'article auquel vous tentez d'accéder n'existe pas."
+        }
+        res.render('article.pug', { article })
+    }).catch(error => res.send(error))
 });
 blogRouter.get('/admin', (request, response) =>{
     response.send('Hello Admin Jo !');
